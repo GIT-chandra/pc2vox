d8ec71390216f3f27faf86b1feeedb506587359f
Angom Geetchandra <angomgeetchandra@gmail.com>
2018-02-11 19:03:48 +0530

    voxelize.py currently working properly

--- voxelizeFill.py
+++ voxelizeFill.py
@@ -1,10 +1,10 @@
-import time, trimesh, os, pymesh
+import time, trimesh
 import numpy as np
 
-FILE = 'model.obj'
-DIM = 64
+FILE = 'bunny-flatfoot.stl'
+DIM = 32
 EXTENT = 100.0
-CLEARANCE = 2.0 # space between the model and the bounds of grid
+CLEARANCE = 5.0 # space between the model and the bounds of grid
 
 coords = np.linspace(-EXTENT/2,EXTENT/2,DIM + 1)
 unit = EXTENT/DIM     # side of a cube
@@ -50,7 +50,86 @@ def areCornersIn(xy_inds,lsegs):
 
     return np.remainder(intersectionsSum,2)
 
-orig_mesh = pymesh.load_mesh(FILE)
+def safeInd(val):
+    return max(DIM - 1,min(0,val))
+
+def getVoxInd(coord):
+    return ((coord + EXTENT/2)*DIM/EXTENT - 1).astype(int)
+
+def findLeftRight(lseg):
+    x1, x2, y1, y2 = lseg[0,0],lseg[1,0],lseg[0,1],lseg[1,1]
+    x, y = x2 - x1, y2 - y1
+    if (abs(y) > abs(x)):
+        return np.array([1,0])
+    else:
+        return np.array([0,1])
+
+def sign(a):
+    if a<0:
+        return -1
+    elif a>0:
+        return 1
+    return 0
+
+def setBoundaryPixels(lseg,vz):
+    x1, x2, y1, y2 = lseg[0,0],lseg[1,0],lseg[0,1],lseg[1,1]
+
+    dx, dy, x, y = abs(x2 - x1), abs(y2 - y1), x1, y1
+    S1, S2 = sign(x2 - x1), sign(y2 - y1)
+
+    if dy>dx:
+        t = dx
+        dx = dy
+        dy = dx
+        interchanged = 1
+    else:
+        interchanged = 0
+
+    E, A, B = 2 * dy - dx, 2 * dy, 2 * dy - 2 * dx
+
+    for i in range(dx):
+        if E<0:
+            if interchanged == 1:
+                y += S2
+            else:
+                x += S1
+            E += A
+        else:
+            y += S2
+            x += S1
+            E += B
+        inOutGrid[x,y,vz] = 1
+        inOutGrid[x + 1,y,vz] = 1
+        inOutGrid[x,y + 1,vz] = 1
+        inOutGrid[x + 1,y + 1,vz] = 1
+
+def boundaryFill(seedx,seedy,z):
+    Q = [(seedx,seedy)]
+    while len(Q)>0:
+        seed = Q[0]
+        # print(len(Q))
+        Q.remove(Q[0])
+        # print(len(Q))
+        # print(seed)
+        if min(seed) < 0 | max(seed) > DIM:
+            # print('overflow')
+            continue
+        # if (inOutGrid[seed[0],seed[1],z] == 1):
+            # print('One!')
+        if (inOutGrid[seed[0],seed[1],z] == 0):
+            inOutGrid[seed[0],seed[1],z] = 1
+            if min(seed) > 1 | max(seed) < DIM - 1:
+                if (inOutGrid[seed[0] - 1,seed[1],z] == 0):
+                    Q.append((seed[0] - 1,seed[1]))
+                if (inOutGrid[seed[0] + 1,seed[1],z] == 0):
+                    Q.append((seed[0] + 1,seed[1]))
+                if (inOutGrid[seed[0],seed[1] - 1,z] == 0):
+                    Q.append((seed[0],seed[1] - 1))
+                if (inOutGrid[seed[0],seed[1] + 1,z] == 0):
+                    Q.append((seed[0],seed[1] + 1))
+
+
+orig_mesh = trimesh.load_mesh(FILE)
 verts = np.array(orig_mesh.vertices)
 
 # centering the model
@@ -68,7 +147,6 @@ mesh = trimesh.base.Trimesh(verts,orig_mesh.faces)
 
 start_time = time.time()
 
-
 # find interior cube corners for every layer
 z_ind = 0
 for z in coords:
@@ -81,28 +159,47 @@ for z in coords:
         z_ind += 1
         continue
 
-    xs = np.linspace(0, DIM, DIM + 1)
-    xy = np.concatenate([np.repeat(xs,DIM + 1).reshape((-1,1)),
-                        np.tile(xs,DIM + 1).reshape((-1,1))],axis = 1)
+    # line segments pf the polygon, in the form of indices of the cubes representing the end points
+    voxs = getVoxInd(cut_slice)
 
-    xyInOut = areCornersIn(xy,cut_slice)
-    non_zeros = xyInOut.nonzero()
+    prospectiveSeeds = []
 
-    xyInsides = xy[non_zeros[0]].astype(int)
-    inOutGrid[xyInsides[:,0],xyInsides[:,1],np.repeat(z_ind,xyInsides.shape[0])] = 1
+    # for each line segment, collect corners to be tested [those within bounding box of segment]
+    for i in range(voxs.shape[0]):
+        '''
+        Here, x and y refer to indices, not actual coordinates.
+        So, xy contains list of indices of the cube corners which must be checked
+        '''
+        lseg = voxs[i,:,:]
+        mids = (lseg[0,:] + lseg[1,:])/2
+
+        vec = findLeftRight(lseg)
+        prospectiveSeeds.append(mids + vec)
+        prospectiveSeeds.append(mids - vec)
+        prospectiveSeeds.append(mids + 2*vec)
+        prospectiveSeeds.append(mids - 2*vec)
+        prospectiveSeeds.append(mids + 3*vec)
+        prospectiveSeeds.append(mids - 3*vec)
+
+        setBoundaryPixels(lseg,z_ind)
+
+    prospectiveSeeds = np.array(prospectiveSeeds)
+    seedsInOut = areCornersIn(prospectiveSeeds,cut_slice)
+    non_zeros = seedsInOut.nonzero()
+
+    seeds = prospectiveSeeds[non_zeros[0]].astype(int)
+
+    print(z_ind)
+    # boundaryFill(0,0, z_ind)
+    # for seed in seeds:
+    #     print(seed)
+    #     if inOutGrid[seed[0],seed[1], z_ind] == 0:
+    #         print(seed)
+    #         boundaryFill(seed[0],seed[1], z_ind)
+
+
 
     z_ind += 1
-
-# toBeProbed = np.array(probeMap.nonzero()).transpose()
-# # order of powers of 2 based on matching with the convention used in the online
-# # configurations sheet
-# powers = np.tile(np.array([2,1,32,16,4,8,64,128]),toBeProbed.shape[0])
-# zero1 = np.array([0,1])
-# addend = np.array([np.repeat(zero1,4),np.tile(np.repeat(zero1,2),2),np.tile(zero1,4)])
-# temp_ = np.repeat(toBeProbed,8, axis = 0)
-# addend = np.tile(addend.transpose(),(toBeProbed.shape[0],1))
-# indices = (temp_ + addend).astype(int)
-# val = np.sum((inOutGrid[indices[:,0],indices[:,1],indices[:,2]]*powers).reshape(-1,8), axis = 1).astype(int)
 
 
 powers = np.tile(np.array([2,1,32,16,4,8,64,128]),DIM**3)
@@ -119,38 +216,30 @@ val = np.sum((inOutGrid[indices[:,0],indices[:,1],indices[:,2]]*powers).reshape(
 
 
 
-
-
-if os.path.exists('triPts' + str(DIM) + '.npy') == True:
-    triPts = np.load('triPts' + str(DIM) + '.npy')
-else:
-    triPts = np.empty((DIM,DIM,DIM,12,3))
-    u2 = unit/2.0
-    '''
-    values to be added to base corner of each cube,
-    to get the vertices which will form the triangles of the mesh
-    '''
-    triAddend = np.array([[0,0,u2],
-                        [u2,0,0],
-                        [unit,0,u2],
-                        [u2,0,unit],
-                        [0,unit,u2],
-                        [u2,unit,0],
-                        [unit,unit,u2],
-                        [u2,unit,unit],
-                        [0,u2,unit],
-                        [0,u2,0],
-                        [unit,u2,0],
-                        [unit,u2,unit]])
-    for x in range(DIM):
-        for y in range(DIM):
-            for z in range(DIM):
-                triPts[x,y,z] = np.tile(
-                                np.array([coords[x],coords[y],coords[z]]).reshape(1,3),
-                                (12,1)) + triAddend
-    np.save('triPts' + str(DIM) + '.npy',triPts)
-
-
+triPts = np.empty((DIM,DIM,DIM,12,3))
+u2 = unit/2.0
+'''
+values to be added to base corner of each cube,
+to get the vertices which will form the triangles of the mesh
+'''
+triAddend = np.array([[0,0,u2],
+                    [u2,0,0],
+                    [unit,0,u2],
+                    [u2,0,unit],
+                    [0,unit,u2],
+                    [u2,unit,0],
+                    [unit,unit,u2],
+                    [u2,unit,unit],
+                    [0,u2,unit],
+                    [0,u2,0],
+                    [unit,u2,0],
+                    [unit,u2,unit]])
+for x in range(DIM):
+    for y in range(DIM):
+        for z in range(DIM):
+            triPts[x,y,z] = np.tile(
+                            np.array([coords[x],coords[y],coords[z]]).reshape(1,3),
+                            (12,1)) + triAddend
 
 # vx = inOutGrid.nonzero()[0]
 # vy = inOutGrid.nonzero()[1]
@@ -183,8 +272,7 @@ with open('test.stl','w') as stlFile:
             v1 = triPts[xyz[0],xyz[1],xyz[2],triAng[0],:]
             v2 = triPts[xyz[0],xyz[1],xyz[2],triAng[1],:]
             v3 = triPts[xyz[0],xyz[1],xyz[2],triAng[2],:]
-            n = np.cross(v2 - v1, v3 - v1)
-            stlFile.write('\tfacet normal ' + str(n[0]) + ' ' + str(n[1]) + ' ' + str(n[2]) + '\n ')
+            stlFile.write('\tfacet normal 0 0 1\n')
             stlFile.write('\t\touter loop\n')
             stlFile.write('\t\t\tvertex ' + str(v1[0]) + ' ' + str(v1[1]) + ' ' + str(v1[2]) + '\n ')
             stlFile.write('\t\t\tvertex ' + str(v2[0]) + ' ' + str(v2[1]) + ' ' + str(v2[2]) + '\n ')
