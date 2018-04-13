348eb0426fbf02cbf4352e5acf863bf5b0be267d
Angom Geetchandra <angomgeetchandra@gmail.com>
2018-04-14 00:19:11 +0530

    Started work on slice view method.

--- classifier_slice.py
+++ classifier_slice.py
@@ -1,5 +1,12 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+from enum import Enum
+
+class axis(Enum):
+    x = 0
+    y = 1
+    z = 2
 
 import trimesh
 
@@ -10,8 +17,8 @@ LEARNING_RATE = 5e-6
 B1 = 0.9
 B2 = 0.9
 
-BATCH_TRAIN = 6
-BATCH_EVAL = 8
+BATCH_TRAIN = 16
+BATCH_EVAL = 24
 
 eval_start_delay = 600
 eval_delay = 600
@@ -21,9 +28,9 @@ REG_PARAM = 0.05
 def triangle_area(v1,v2,v3):
     return 0.5 * np.linalg.norm(np.cross(v2-v1, v3-v1),axis = 1)
 
-def createGrid(mesh, random_num_pts = False, n = 2**11, use_seed = False):
+def get_image(mesh, img_axis, img_res=256, random_num_pts = False, n = 2**12, use_seed = False):
     if use_seed:
-        np.random.seed(8)
+        np.random.seed(2)
     faces = mesh.faces
     v1s,v2s,v3s = mesh.vertices[faces[:,0],:],mesh.vertices[faces[:,1],:],mesh.vertices[faces[:,2],:]
 
@@ -47,12 +54,11 @@ def createGrid(mesh, random_num_pts = False, n = 2**11, use_seed = False):
 
     u[invalids] = 1 - u[invalids]
     v[invalids] = 1 - v[invalids]
-
     w = 1-(u+v)
 
-    pt_cld = (sel_v1s * u) + (sel_v2s * v) + (sel_v3s * w)
+    pt_cld = (sel_v1s * u) + (sel_v2s * v) + (sel_v3s * w)   
 
-    # centering the model
+     # centering the model
     delta = (np.max(pt_cld,axis=0) + np.min(pt_cld,axis=0))/2.0
     if sum(np.abs(delta)) != 0.0:
         pt_cld -= delta
@@ -61,24 +67,29 @@ def createGrid(mesh, random_num_pts = False, n = 2**11, use_seed = False):
     span = np.max(np.max(pt_cld,axis=0) - np.min(pt_cld,axis=0))
     pt_cld /= span
     pt_cld *= (EXTENT - 2*CLEARANCE)
-    pt_cld += EXTENT/2.0    # to get indices correctly on dividing by unit length
+    pt_cld += EXTENT/2.0    # to get indices correctly on dividing by unit length 
+
+    # getting the image 
+    unit = EXTENT/float(img_res)
+    
+    inds_xyz = (pt_cld/unit).astype(int)
+    inds_img = inds_xyz[np.where(inds_xyz[:,img_axis.value] == int(img_res/2))[0]]
+    for i in range(20):
+        inds_img = np.concatenate([inds_img, inds_xyz[np.where(inds_xyz[:,img_axis.value] == int(img_res/2 - i))[0]] ],axis = 0)
+        inds_img = np.concatenate([inds_img, inds_xyz[np.where(inds_xyz[:,img_axis.value] == int(img_res/2 + i))[0]] ],axis = 0)
+    img = np.zeros((img_res,img_res))
+
+    img_axes_map = {0:[2,1],1:[2,0],2:[0,1]}
+    for pix in inds_img:
+        img[pix[img_axes_map[img_axis.value][0]], pix[img_axes_map[img_axis.value][1]]] = 1
+    # max_intensity = np.max(img)
+    # if  max_intensity != 0:
+    #     img /= max_intensity
+    
+    return img
 
 
-    matrices = [np.zeros((128,128,128)),
-    np.zeros((64,64,64)),
-    np.zeros((32,32,32)),
-    np.zeros((16,16,16)),
-    np.zeros((8,8,8)),
-    np.zeros((4,4,4))]
 
-    for D in range(len(matrices)):
-        unit = EXTENT/float(2**(len(matrices) - D + 1))
-        inds = (pt_cld/unit).astype(int)
-        for i in inds:
-            matrices[D][tuple(i)] = 1
-        matrices[D] /= np.mean(matrices[D])
-
-    return np.array(matrices)
 
 TRAIN_FILE_LIST = 'trainFilesShuffled.txt'
 EVAL_FILE_LIST = 'evalFiles.txt'
@@ -107,26 +118,12 @@ tf.logging.set_verbosity(tf.logging.INFO)
 def Convo3d(layer,num_filters, isTrain):
     cnv =  tf.layers.conv3d(layer,num_filters,[5,5,5],padding = 'same',\
     activation = tf.nn.relu, kernel_initializer = tf.contrib.layers.xavier_initializer())
-
-    # cnv =  tf.layers.conv3d(layer,num_filters,[5,5,5],padding = 'same',\
-    # activation = tf.nn.relu, kernel_initializer = tf.contrib.layers.xavier_initializer(),\
-    # kernel_regularizer=tf.contrib.layers.l1_regularizer(scale=L2PARAM))
-
-    # cnv = tf.contrib.layers.batch_norm(cnv, is_training=isTrain)
-    # return tf.nn.dropout(cnv,0.5)
     return cnv
 
 def upConvo3d(layer,num_filters, isTrain):
     cnv =  tf.layers.conv3d_transpose(layer,num_filters,[2,2,2],strides = (2,2,2),\
     activation = tf.nn.relu,kernel_initializer = tf.contrib.layers.xavier_initializer(),\
      use_bias = False)
-
-    # cnv =  tf.layers.conv3d_transpose(layer,num_filters,[2,2,2],strides = (2,2,2),\
-    # activation = tf.nn.relu, kernel_initializer = tf.contrib.layers.xavier_initializer(), \
-    # kernel_regularizer=tf.contrib.layers.l1_regularizer(scale=L2PARAM),use_bias = False)
-
-    # cnv = tf.contrib.layers.batch_norm(cnv, is_training=isTrain)
-    # return tf.nn.dropout(cnv,0.5)
     return cnv
 
 def Mpool(layer):
@@ -134,67 +131,28 @@ def Mpool(layer):
 
 def model_fn(features,labels,mode):
     isTrain = (mode == tf.estimator.ModeKeys.TRAIN)
-    # input128 = tf.reshape(features['128'], [-1, 128, 128, 128, 1])
     input64 = tf.reshape(features['64'], [-1, 64, 64, 64, 1])
-    # input32 = tf.reshape(features['32'], [-1, 32, 32, 32, 1])
-    # input16 = tf.reshape(features['16'], [-1, 16, 16, 16, 1])
-    # input8 = tf.reshape(features['8'], [-1, 8, 8, 8, 1])
-    # input4 = tf.reshape(features['4'], [-1, 4, 4, 4, 1])
 
-    # c0a = Convo3d(input256,64, isTrain)
-    # c0b = Convo3d(c0a,32,isTrain)
-    # p0 = Mpool(c0b) # [-1,64,64,64,f]
-
-    # c1a = Convo3d(input128,32, isTrain)
-    # ccat0 = tf.concat([c1a,p0],4)
-    # c1b = Convo3d(ccat0,16,isTrain)
-    # p1 = Mpool(c1b) # [-1,64,64,64,f]
-
-    # c1a = Convo3d(input128,32, isTrain)
-    # c1b = Convo3d(c1a,16,isTrain)
-    # p1 = Mpool(c1b) # [-1,64,64,64,f]
-
-    c2a = Convo3d(input64,16,isTrain)
-    # ccat1 = tf.concat([c2a,p1],4)
-    # c2b = Convo3d(ccat1,16,isTrain)
-    c2b = Convo3d(c2a,16,isTrain)
+    c2a = Convo3d(input64,8,isTrain)
+    c2b = Convo3d(c2a,8,isTrain)
     p2 = Mpool(c2b) # [-1,32,32,32,f]
 
-    c3a = Convo3d(p2,16,isTrain)
-    # ccat2 = tf.concat([c3a,p2],4)
-    c3b = Convo3d(c3a,16,isTrain)
+    c3a = Convo3d(p2,8,isTrain)
+    c3b = Convo3d(c3a,8,isTrain)
     p3 = Mpool(c3b) # [-1,16,16,16,f]
 
-    c4a = Convo3d(p3,16,isTrain)
-    # ccat3 = tf.concat([c4a,p3],4)
-    c4b = Convo3d(c4a,16,isTrain)
+    c4a = Convo3d(p3,8,isTrain)
+    c4b = Convo3d(c4a,8,isTrain)
     p4 = Mpool(c4b) # [-1,8,8,8,f]
 
-    c5a = Convo3d(p4,16,isTrain)
-    # ccat4 = tf.concat([c5a,p4],4)
-    c5b = Convo3d(c5a,16,isTrain)
-    # p5 = Mpool(c5b) # [-1,4,4,4,f]
+    c5a = Convo3d(p4,8,isTrain)
+    c5b = Convo3d(c5a,8,isTrain)
 
-    # c6a = Convo3d(input4,8,isTrain)
-    # ccat5 = tf.concat([c6a,p5],4)
-
-    # c6a = Convo3d(p5,32,isTrain)
-    # c6a = Convo3d(ccat5,64,isTrain)
-    # c6b = Convo3d(c6a,32,isTrain)
-
-    # dense1 = tf.layers.dense(tf.layers.Flatten()(c6b),100,activation=tf.nn.relu)
-    # logits = tf.layers.dense(dense1,NUM_CLASSES)
-
-    # dense1 = tf.layers.dense(tf.layers.Flatten()(c6b),512,activation=tf.nn.relu)
-    dense1 = tf.layers.dense(tf.layers.Flatten()(c5b),128,activation=tf.nn.relu)
+    dense1 = tf.layers.dense(tf.layers.Flatten()(c5b),64,activation=tf.nn.relu)
     logits = tf.layers.dense(dense1,NUM_CLASSES)
-    # dense2 = tf.layers.dense(dense1,128,activation=tf.nn.relu)
-    # logits = tf.layers.dense(dense2,NUM_CLASSES)
 
     if mode != tf.estimator.ModeKeys.PREDICT:
         loss = tf.losses.sparse_softmax_cross_entropy(labels,logits)
-        # loss = tf.losses.sparse_softmax_cross_entropy(labels,logits) + \
-        #     REG_PARAM*tf.losses.get_regularization_loss()
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE, beta1=B1, beta2=B2)
@@ -228,39 +186,23 @@ def train_sample_gen():
     count = -1
     while(1):
         count += 1
-        # print(count)
         if count >= num_files:
             count = 0
-            # print('Epoch done')
-            # print(trainFiles[0:20])
             trainFiles = trainFiles[np.random.permutation(num_files)]
-            # print(trainFiles[0:20])
 
         # preparing the lable file
         cat = trainFiles[count].split('/')[1]
         labels = CAT_DICT[cat]
 
         # Input file
-        # inputFull = np.load(trainFiles[count][:-3] + 'npy', encoding='latin1')
         mesh = trimesh.load_mesh(trainFiles[count])
-
-        # ang_choice = np.random.randint(8)
-        # if ang_choice != 0:
-        #     # angle_rad = ang_choice*np.pi/4.0        
-        #     angle_rad = np.random.rand()*2*np.pi    
-        #     M = trimesh.transformations.rotation_matrix(angle_rad,[0,0,1])
-        #     mesh.apply_transform(M)
+      
+        # angle_rad = np.random.rand()*2*np.pi    
+        # M = trimesh.transformations.rotation_matrix(angle_rad,[0,0,1])
+        # mesh.apply_transform(M)
 
         inputFull = createGrid(mesh)
-        # inputFull = createGrid(mesh, random_num_pts=True)
-
-        yield inputFull[0].astype(np.float32), \
-        inputFull[1].astype(np.float32), \
-        inputFull[2].astype(np.float32), \
-        inputFull[3].astype(np.float32), \
-        inputFull[4].astype(np.float32), \
-        inputFull[5].astype(np.float32), \
-        labels
+        yield inputFull.astype(np.float32), labels
 
 def get_train_inputs(batch_size):
     iterator_initializer_hook = IteratorInitializerHook()
@@ -269,36 +211,71 @@ def get_train_inputs(batch_size):
         with tf.name_scope('Training_data'):
             dataset = tf.data.Dataset.from_generator(
             generator = train_sample_gen,
-            output_types = (tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.int64),
-            output_shapes= ([128,128,128], [64,64,64], [32,32,32], [16,16,16], [8,8,8], [4,4,4], []))
+            output_types = (tf.float32, tf.int64),
+            output_shapes= ([64,64,64], []))
 
             dataset = dataset.repeat(None)
             dataset = dataset.batch(batch_size)
             # dataset = dataset.shuffle(buffer_size=3500)
 
             iterator = dataset.make_initializable_iterator()
-            inp128, inp64, inp32, inp16, inp8, inp4, label = iterator.get_next()
+            inp64, label = iterator.get_next()
             iterator_initializer_hook.iterator_initializer_func = \
                 lambda sess: sess.run(
                     iterator.initializer)
-            return {'128':inp128, '64':inp64, '32':inp32, '16':inp16, '8':inp8, '4':inp4}, label
+            return {'64':inp64}, label
     return train_inputs, iterator_initializer_hook
 
 def eval_sample_gen():
     with open(EVAL_FILE_LIST,'r') as f:
         evalFiles = np.array(f.read().split('\n')[:-1])
     num_files = evalFiles.shape[0]
-    evalFiles = evalFiles[np.random.permutation(num_files)]
 
     count = -1
     while(1):
         count += 1
-        # print(count)
         if count >= num_files:
-            # print('Epoch of evaluation samples!')
             count = 0
-            evalFiles = evalFiles[np.random.permutation(num_files)]
 
+        # preparing the lable file
+        cat = evalFiles[count].split('/')[1]
+        labels = CAT_DICT[cat]
+
+        # Input file
+        mesh = trimesh.load_mesh(evalFiles[count])
+        inputFull = createGrid(mesh, use_seed=True)
+
+        yield inputFull.astype(np.float32),labels
+
+def get_eval_inputs(batch_size):
+    iterator_initializer_hook = IteratorInitializerHook()
+
+    def eval_inputs():
+        with tf.name_scope('Evaluation_data'):
+            dataset = tf.data.Dataset.from_generator(
+            generator = eval_sample_gen,
+            output_types = (tf.float32, tf.int64),
+            output_shapes= ([64,64,64], []))
+
+            dataset = dataset.repeat(None)
+            dataset = dataset.batch(batch_size)
+
+            iterator = dataset.make_initializable_iterator()
+            inp64, label = iterator.get_next()
+            iterator_initializer_hook.iterator_initializer_func = \
+                lambda sess: sess.run(
+                    iterator.initializer)
+            return {'64':inp64}, label
+    return eval_inputs, iterator_initializer_hook
+
+def pred_sample_gen():
+    with open(EVAL_FILE_LIST,'r') as f:
+        evalFiles = np.array(f.read().split('\n')[:-1])
+    num_files = evalFiles.shape[0]
+
+    count = 0
+    while(count < num_files):
+        
         # preparing the lable file
         cat = evalFiles[count].split('/')[1]
         labels = CAT_DICT[cat]
@@ -308,6 +285,8 @@ def eval_sample_gen():
         mesh = trimesh.load_mesh(evalFiles[count])
         inputFull = createGrid(mesh, use_seed=True)
 
+        count += 1
+
         yield inputFull[0].astype(np.float32), \
         inputFull[1].astype(np.float32), \
         inputFull[2].astype(np.float32), \
@@ -316,13 +295,13 @@ def eval_sample_gen():
         inputFull[5].astype(np.float32), \
         labels
 
-def get_eval_inputs(batch_size):
+def get_pred_inputs(batch_size):
     iterator_initializer_hook = IteratorInitializerHook()
 
-    def eval_inputs():
+    def pred_inputs():
         with tf.name_scope('Evaluation_data'):
             dataset = tf.data.Dataset.from_generator(
-            generator = eval_sample_gen,
+            generator = pred_sample_gen,
             output_types = (tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.int64),
             output_shapes= ([128,128,128], [64,64,64], [32,32,32], [16,16,16], [8,8,8], [4,4,4], []))
 
@@ -335,18 +314,16 @@ def get_eval_inputs(batch_size):
                 lambda sess: sess.run(
                     iterator.initializer)
             return {'128':inp128, '64':inp64, '32':inp32, '16':inp16, '8':inp8, '4':inp4}, label
-    return eval_inputs, iterator_initializer_hook
+    return pred_inputs, iterator_initializer_hook
 
-def pred_inp_fn():
-    inputFull = np.load('/home/raman/DDP_17-18/pc2vox/ModelNet10/chair/train/chair_0001.npy', encoding='latin1')
-    return {'128':inputFull[0].astype(np.float32), \
-        '64':inputFull[1].astype(np.float32), \
-        '32':inputFull[2].astype(np.float32), \
-        '16':inputFull[3].astype(np.float32), \
-        '8':inputFull[4].astype(np.float32), \
-        '4':inputFull[5].astype(np.float32)}, None
+if __name__ == '__main__':
+    FNAME = 'ModelNet10/sofa/test/sofa_0681.off'
+    mesh = trimesh.load_mesh(FNAME)
+    img = get_image(mesh, axis.y)
+    plt.imshow(img, cmap='gray', origin='lower')
+    plt.show()
+    exit()
 
-def main(unused_argv):
     mycfg = tf.estimator.RunConfig(model_dir=None,
     tf_random_seed=None,
     save_summary_steps=100,
@@ -365,7 +342,7 @@ def main(unused_argv):
     train_spec = tf.estimator.TrainSpec(
     input_fn = train_input_fn,
     hooks=[train_input_hook],
-    max_steps=20000)
+    max_steps=40000)
 
     eval_spec = tf.estimator.EvalSpec(
     input_fn = eval_input_fn,
@@ -374,44 +351,41 @@ def main(unused_argv):
     throttle_secs=eval_delay,
     start_delay_secs=eval_start_delay
     )
-    # eval_spec = tf.estimator.EvalSpec(
-    # input_fn = train_input_fn,
-    # hooks=[train_input_hook],
-    # throttle_secs=10,
-    # start_delay_secs=10
-    # )
 
     tf.estimator.train_and_evaluate(est, train_spec, eval_spec)
 
-    # inputFull = np.load('ModelNet10/bed/test/bed_0516.npy', encoding='latin1')
-    # pred_input_fn = tf.estimator.inputs.numpy_input_fn(\
-    # x = {'128':inputFull[0].astype(np.float32), \
-    #     '64':inputFull[1].astype(np.float32), \
-    #     '32':inputFull[2].astype(np.float32), \
-    #     '16':inputFull[3].astype(np.float32), \
-    #     '8':inputFull[4].astype(np.float32), \
-    #     '4':inputFull[5].astype(np.float32)},
-    # y = None,
-    # batch_size=1,
-    # num_epochs=1,
-    # shuffle=False)
+    # pred_input_fn, pred_input_hook = get_pred_inputs(batch_size = BATCH_EVAL)
+    # res = est.predict(input_fn = pred_input_fn, hooks=[pred_input_hook])
+    
+    # labels = np.zeros(NUM_FILES_EVAL,dtype=int)
+    # with open(EVAL_FILE_LIST,'r') as f:
+    #     evalFiles = np.array(f.read().split('\n')[:-1])    
+    # count = 0
+    # print('Preparing labels')
+    # while(count < NUM_FILES_EVAL):       
+    #     cat = evalFiles[count].split('/')[1]
+    #     labels[count] = int(CAT_DICT[cat])
+    #     count += 1
+    # print('Done labels')
 
-
-    # res = est.predict(input_fn = pred_inp_fn)
+    # confusion_matrix = np.zeros((NUM_CLASSES,NUM_CLASSES))
+    # count = 0
     # for r in res:
-    #     voxx = r['predictions']
-    #     break
-    # np.save('pred.npy',voxx)
-    #
-    # voxx = np.load('pred.npy').reshape(128,128,128)
-    #
-    # import matplotlib.pyplot as plt
-    # from mpl_toolkits.mplot3d import Axes3D
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111,projection='3d')
-    # idxs = np.where(voxx > 0.5)
-    # ax.scatter(idxs[0],idxs[1],idxs[2],c='red')
-    # plt.show()
+    #     pred = int(r)
+    #     print('Sample',count,'prediction : ',pred,'GT : ',labels[count])
 
-if __name__ == '__main__':
-    tf.app.run()
+    #     confusion_matrix[labels[count],pred] += 1
+
+    #     count += 1
+    #     if count >= NUM_FILES_EVAL:
+    #         break
+
+    # tp = 0
+    # for i in range(NUM_CLASSES):
+    #     tp += confusion_matrix[i,i]
+    # print('Accuracy',tp/NUM_FILES_EVAL)
+
+
+    # plt.imshow(confusion_matrix, cmap='gray')
+    # plt.colorbar()
+    # plt.show()
